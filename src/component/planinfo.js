ef5b3749708d3dac7b05b58a64c3f68dee3939f4
baojiajie <382058502@qq.com>
2016-02-26 12:53:25 +0800

    '123'

--- src/component/planinfo.js
+++ src/component/planinfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon,Row,Col,Button } from 'antd';
+import { Icon,Row,Col,Button,Alert,Tooltip  } from 'antd';
 
 const Planinfo = React.createClass({
   getInitialState() {
@@ -18,10 +18,12 @@ const Planinfo = React.createClass({
           <p id="text_createtime">创建时间：<span>2015-02-25 16:16:23</span></p>
           <p id="text_plandetail">预案描述：<span>系统无法正常运行，需要及时解决</span></p>
 
-          <Button id="btn_plantest" type="ghost" size="large">
-            <Icon type="caret-circle-o-right" />
-          预案演练
-          </Button>
+          <Tooltip placement="top" title="您可以点击进行预演">
+            <Button id="btn_plantest" type="ghost" size="large">
+              <Icon type="caret-circle-o-right" />
+            预案演练
+            </Button>
+          </Tooltip>
           <Button id="btn_planstart" type="primary" size="large">
             <Icon type="caret-circle-o-right" />
           预案执行
@@ -29,6 +31,43 @@ const Planinfo = React.createClass({
         </div>
         <span style={{margin:'30px 4%',width:'92%'}} className="ant-line" ></span>
         <div id='planstep'>
+          <Row type="flex" justify="start">
+            <Col span="1" offset="1">
+              <Icon style={{fontSize:'24px',marginTop:'20px'}} type="tags-o" />
+            </Col>
+            <Col span="16" offset="1" >
+              <Alert message="打电话" description="通知所有技术员工进场修复问题通题" type="info" />
+              <a className="btnDetail"><Icon type="file-text" /></a>
+            </Col>
+          </Row>
+          <Row type="flex" justify="start">
+            <Col span="1" offset="1">
+              <Icon style={{fontSize:'24px',marginTop:'20px'}} type="tags-o" />
+            </Col>
+            <Col span="16" offset="1" >
+              <Alert message="打电话" description="通知所有技术员工进场修复问题通题" type="info" />
+              <a className="btnDetail"><Icon type="file-text" /></a>
+            </Col>
+          </Row>
+          <Row type="flex" justify="start">
+            <Col span="1" offset="1">
+              <Icon style={{fontSize:'24px',marginTop:'20px'}} type="tags-o" />
+            </Col>
+            <Col span="16" offset="1" >
+              <Alert message="打电话" description="通知所有技术员工进场修复问题通题" type="info" />
+              <a className="btnDetail"><Icon type="file-text" /></a>
+            </Col>
+          </Row>
+          <Row type="flex" justify="start">
+            <Col span="1" offset="1">
+              <Icon style={{fontSize:'24px',marginTop:'20px'}} type="tags-o" />
+            </Col>
+            <Col span="16" offset="1" >
+              <Alert message="打电话" description="通知所有技术员工进场修复问题通题" type="info" />
+              <a className="btnDetail"><Icon type="file-text" /></a>
+            </Col>
+          </Row>
+
         </div>
       </div>
     );
--- src/component/planpanel.js
+++ src/component/planpanel.js
@@ -1,15 +1,55 @@
 import React from 'react';
-import { Menu, Icon,Row,Col,Button } from 'antd';
+import { Menu, Icon,Row,Col,Button,Modal,Popconfirm,Form,Input } from 'antd';
 const SubMenu = Menu.SubMenu;
 const ButtonGroup = Button.Group;
+const createForm = Form.create;
+const FormItem = Form.Item;
 const Planinfo = require('./planinfo');
 
 
-const Planpanel = React.createClass({
+var Planpanel = React.createClass({
+  getValidateStatus(field) {
+    const { isFieldValidating, getFieldError, getFieldValue } = this.props.form;
+
+    if (isFieldValidating(field)) {
+      return 'validating';
+    } else if (!!getFieldError(field)) {
+      return 'error';
+    } else if (getFieldValue(field)) {
+      return 'success';
+    }
+  },
+  checkPass(rule, value, callback) {
+    const { validateFields } = this.props.form;
+    if (value) {
+      validateFields(['rePasswd']);
+    }
+    callback();
+  },
+
+  checkPass2(rule, value, callback) {
+    const { getFieldValue } = this.props.form;
+    if (value && value !== getFieldValue('passwd')) {
+      callback('两次输入密码不一致！');
+    } else {
+      callback();
+    }
+  },
   getInitialState() {
     return {
-      current: "4"
+      current: "4",
+      visible: false
     };
+  },
+  handleAddPlan() {
+    this.setState({
+      visible: true
+    });
+  },
+  handleAddGroup() {
+    this.setState({
+      visible: true
+    });
   },
   handleClick(e) {
     console.log('click ', e);
@@ -17,7 +57,36 @@ const Planpanel = React.createClass({
       current: e.key
     });
   },
+  handleOk() {
+    this.setState({
+      ModalText: '对话框将在两秒后关闭',
+      confirmLoading: true
+    });
+    setTimeout(() => {
+      this.setState({
+        visible: false,
+        confirmLoading: false
+      });
+    }, 2000);
+  },
+  handleCancel() {
+    console.log('点击了取消');
+    this.setState({
+      visible: false
+    });
+  },
   render() {
+    const { getFieldProps, getFieldError, isFieldValidating } = this.props.form;
+    const nameProps = getFieldProps('name', {
+      rules: [
+        { required: true, min: 4, message: '名称至少为 4 个字符' },
+        { validator: this.userExists },
+      ],
+    });
+    const formItemLayout = {
+      labelCol: { span: 7 },
+      wrapperCol: { span: 12 },
+    };
     return (
         <Row id="planpanel">
           <Col id="planlist" span="5" className="panel" >
@@ -40,27 +109,61 @@ const Planpanel = React.createClass({
             </Menu>
             <div id="operatebtngroup">
               <ButtonGroup size="large">
-                <Button type="primary">
-                  <Icon type="plus" />
-                  增加
-                </Button>
+                <Popconfirm title="请问您要添加分组还是预案？"
+                onConfirm={this.handleAddPlan} onCancel={this.handleAddGroup} okText="预案" cancelText="分组">
+                  <Button type="primary">
+                    <Icon type="plus" />
+                    增加
+                  </Button>
+                </Popconfirm>
                 <Button type="ghost">
                   <Icon type="edit" />
                   修改
                 </Button>
-                <Button type="ghost">
-                  <Icon type="delete" />
-                  删除
-                </Button>
+                <Popconfirm title="确定要删除吗？" >
+                  <Button type="ghost">
+                    <Icon type="delete" />
+                    删除
+                  </Button>
+                </Popconfirm>
               </ButtonGroup>
             </div>
           </Col>
           <Col span="19" className="panel">
             <Planinfo />
           </Col>
+          <Modal title="新建预案"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            confirmLoading={this.state.confirmLoading}
+            onCancel={this.handleCancel}>
+            <Form horizontal form={this.props.form}>
+              <FormItem
+                {...formItemLayout}
+                label="预案名称："
+                hasFeedback
+                help={isFieldValidating('name') ? '校验中...' : (getFieldError('name') || []).join(', ')} required>
+                <Input {...nameProps} placeholder="请输入预案名称" />
+              </FormItem>
+              <FormItem
+                {...formItemLayout}
+                label="预案描述：" required>
+                <Input type="textarea" placeholder="请输入预案描述" id="control-textarea" rows="3" />
+              </FormItem>
+              <FormItem>
+                <span style={{margin:'10px 12%',width:'50%'}} className="ant-line" ></span>
+                <Button id="btnaddstep" type="primary" size='small'>
+                  <Icon type="tags-o" />
+                  新建步骤
+                </Button>
+              </FormItem>
+            </Form>
+          </Modal>
         </Row>
     );
   },
 });
+
+Planpanel = createForm()(Planpanel);
 
 export default Planpanel;
